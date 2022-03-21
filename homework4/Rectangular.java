a43509a811b818f0a12635d461c436e7d193910e
SenitaI98 <senita.isovic@live.com>
2022-03-21 22:47:16 +0100

    updated (second commit)

--- homework4/Rectangular.java
+++ homework4/Rectangular.java
@@ -4,21 +4,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Rectangular extends Shape {
-    protected ArrayList<Double> userEntries;
+    private double sideA;
+    private double sideB;
 
-    protected Rectangular(ArrayList<Double> userEntries) {
-        this.userEntries = userEntries;
+    protected Rectangular(double sideA, double sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
     }
 
     @Override
     public double area() {
-        System.out.println("Calculate the area of a rectangular");
-        inputRectangularSides();
-        double areaOfARectangular = 1;
-        for (double sideLength : this.userEntries) {
-            areaOfARectangular *= sideLength;
-        }
-        clearArrayList();
+        double areaOfARectangular = sideA * sideB;
         if (areaOfARectangular < 0) {
             throw new NumberFormatException(areaOfARectangular + " --- area of the rectangular can't be a negative number");
         }
@@ -27,30 +23,10 @@ public class Rectangular extends Shape {
 
     @Override
     public double perimeter() {
-        System.out.println("Calculate the perimeter of a rectangular");
-        inputRectangularSides();
-        double perimeterOfARectangular = 0;
-        for (double sideLength : this.userEntries) {
-            perimeterOfARectangular += sideLength;
-        }
-        clearArrayList();
+        double perimeterOfARectangular = 2 * (sideA + sideB);
         if (perimeterOfARectangular < 0) {
             throw new NumberFormatException(perimeterOfARectangular + " --- perimeter of the rectangular can't be a negative number");
         }
         return perimeterOfARectangular;
-    }
-
-    void inputRectangularSides() {
-        Scanner reader = new Scanner(System.in);
-        int k = 1;
-        do {
-            System.out.println("Please enter the length of rectangular side number " + k);
-            this.userEntries.add(reader.nextDouble());
-            k++;
-        } while (k <= 2);
-    }
-
-    void clearArrayList() {
-        this.userEntries.clear();
     }
 }
